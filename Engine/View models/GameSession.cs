a6a78f2ed92c5a137f2087288e0cd5c4d74835fc
0stapKO <ostap22lviv@gmail.com>
2025-06-11 11:26:55 +0300

    player class added

---
+++ Engine/View models/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.View_models
+{
+    internal class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Hero";
+            CurrentPlayer.Class = "Warrior";
+            CurrentPlayer.HitPoints = 100;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.Gold = 50;
+        }
+    }
+}
--- game/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ game/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace game {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/game;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/game;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
